38ca3ad4c5b79b7a6eb7c9b6c45c665ecb813670
andonilosada <70845025+andonilosada@users.noreply.github.com>
2021-03-02 11:33:48 +0100

    3º commit para clase Intervalo

--- ooEscuelaIT/src/Intervalo.java
+++ ooEscuelaIT/src/Intervalo.java
@@ -1,13 +1,89 @@
 
 class Intervalo {
 	
-	public double longitud()
-	public void desplazar(double desplazamiento)
-	public Intervalo desplazado(double desplazamiento)
-	public boolean incluye(double punto)
-	public boolean equals(Intervalo intervalo)
-	public Intervalo interseccion(Intervalo intervalo)
-	public boolean intersecta(Intervalo intervalo)
+	private double puntoMedio;
 	
-
+	private double longitud;
+	
+	
+	public Intervalo(double inferior, double superior) {
+		
+		longitud = superior - inferior;
+		puntoMedio = inferior + longitud/2;
+	
+	}
+	
+	public Intervalo(Intervalo intervalo) {
+		
+	}
+	
+	public Intervalo() {
+		
+	}
+	
+	public Intervalo(double superior) {
+		
+	}
+	
+	public double longitud() {
+	
+		return longitud;
+	}
+	
+	public double puntoMedio() {
+		
+		return puntoMedio;
+	}
+	
+	public Intervalo clone() {
+		
+		return null;
+	}
+	
+	public void desplazar(double desplazamiento) {
+		
+		puntoMedio += desplazamiento;
+		
+	}
+	
+	public Intervalo desplazado(double desplazamiento) {
+		
+		return null;
+	}
+	
+	public boolean isInclude(double punto) {
+		return true;
+		
+	}
+	
+	public boolean isEquals(Intervalo intervalo) {
+		return true;
+	}
+	
+	public Intervalo interseccion(Intervalo intervalo) {
+		
+		return null;
+	}
+	
+	public boolean isIntersecta(Intervalo intervalo) {
+		
+		return true;
+	}
+	
+	public Intervalo[] trocear(int trozos) {
+		
+		return null;
+	}
+	
+	public void mostrar() {
+		
+		//System.out.println("["+ inferior + "," + superior + "]");
+	}
+	
+	public void doblar() {
+		
+		longitud *= 2;			
+		
+	}
+	
 }
