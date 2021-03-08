2fc6ebc33dab41244a255adea0ff2f571e5284f1
andonilosada <70845025+andonilosada@users.noreply.github.com>
2021-03-08 10:23:11 +0100

    Añado metodo isLeapYear
    Añado un metoddo que controla cuando unn año es bisiesto. Añado otro método que devuelve el dia de la semana cuando le paso una fecha.

--- ooEscuelaIT/src/Fecha.java
+++ ooEscuelaIT/src/Fecha.java
@@ -1,16 +1,64 @@
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.StringTokenizer;
+
 
 public class Fecha {
 	
-	public Fecha() {
-		
-	}
+///////////////////////////////////////////////////////////// ATRIBUTOS
+	
+	private int dia;
+	
+	private int mes;
+	
+	private int año;
+	
+	private final static int[] DIAS_MESES = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	
+///////////////////////////////////////////////////////////// CONSTRUCTORES
 	
 	public Fecha(int dia, int mes, int año) {
-		
+		this.set(dia, mes, año);
 	}
 	
 	public Fecha(Fecha fecha) {
-		
+		this(fecha.dia, fecha.mes, fecha.año);
+	}
+	
+	public Fecha() {
+		this(1,1,1);
+	}
+	
+	public Fecha(String fecha) {
+		StringTokenizer analizador = new StringTokenizer(fecha, "/");
+		dia = Integer.parseInt(analizador.nextToken());
+		mes = Integer.parseInt(analizador.nextToken());
+		año = Integer.parseInt(analizador.nextToken());
+	}
+	
+/////////////////////////////////////////////////////////////// MÉTODOS 
+	
+	public int getDia() {
+		return dia;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public int getAño() {
+		return año;
+	}
+	
+	private void set(int dia, int mes, int año) {
+		this.dia = dia;
+		this.mes = mes;
+		this.año = año;
+	}
+
+	public void set(Fecha fecha) {
+		this.set(fecha.dia, fecha.mes, fecha.año);
 	}
 	
 	public char diaSemana() {
@@ -28,19 +76,11 @@ public class Fecha {
 		return false;
 	}
 	
-	public int diferencia(Fecha fecha) {
-		
-		return 0;
-	}
-	
 	public void mostrar() {
+		System.out.println(dia + "/" + mes + "/" + año);
 		
 	}
 	
-	public void set(Fecha fecha) {
-		
-	}
-
 	public int numeroSemana() {
 		
 		return 0;
@@ -49,15 +89,61 @@ public class Fecha {
 	public void incrementar(int dias) {
 		
 	}
-	
-	public boolean esBisiesto() {
 		
-		return false;
-	}
-	
 	public Fecha clone() {
-		
-		return null;
+		return new Fecha(this);
 	}
 	
+	public boolean igual(Fecha fecha) {
+		return dia == fecha.dia && mes == fecha.mes && año == fecha.año;  
+	}
+	
+	public boolean anterior(Fecha fecha) {
+		return this.diasOrigen() < fecha.diasOrigen();
+	}
+	
+	public boolean posterior(Fecha fecha) {
+		return this.diasOrigen() > fecha.diasOrigen();
+	}
+	
+	public int diferencia(Fecha fecha) {
+		
+		return this.diasOrigen() - fecha.diasOrigen();
+	}
+	
+	public int diasOrigen() {
+		int resultado = dia;
+		for (int i = 0; i < mes - 1; i++) {
+			resultado += DIAS_MESES[i];
+		}
+		resultado += 365 * (año - 1);
+		return resultado;
+	}
+	
+	public static boolean isLeapYear(int year) {
+		  Calendar cal = Calendar.getInstance();
+		  cal.set(Calendar.YEAR, year);
+		  return cal.getActualMaximum(Calendar.DAY_OF_YEAR) > 365;
+	}
+	
+	public static int getDayOfTheWeek(Date d){
+		GregorianCalendar cal = new GregorianCalendar();
+		cal.setTime(d);
+		return cal.get(Calendar.DAY_OF_WEEK);		
+	}
+
+	
+/////////////////////////////////////////////////////////////// MÉTODO MAIN
+	
+	public static void main(String[] args) {
+		
+		//Fecha fecha = new Fecha(25,06,1962);
+		System.out.println(Fecha.isLeapYear(2020));
+	}
 }
+
+
+
+
+
+
--- ooEscuelaIT/src/Main.java
+++ ooEscuelaIT/src/Main.java
@@ -2,12 +2,10 @@
 public class Main {
 	
 	public static void main(String[]args) {
-		
-		Intervalo intervalo = new Intervalo(2.0, 5.0);
-        intervalo.mostrar();
-        intervalo.doblar();
-        intervalo.mostrar();
-	
+	    Intervalo intervalo = new Intervalo();
+	    intervalo.recoger();
+	    intervalo.mostrar();	
 	}
 
 }
+
